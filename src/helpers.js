f16b3a69bb624ea004096a2660f9227b1842a74f
Kanika Gera <kanika.gera2@gmail.com>
2019-12-13 10:00:10 +0100

    Initial commit

---
+++ src/helpers.js
@@ -0,0 +1,62 @@
+/*
+Copyright 2019 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const loggerNamespace = '@{{REPO}}'
+const logger = require('@adobe/aio-lib-core-logging')(loggerNamespace, { level: process.env.LOG_LEVEL })
+
+/** Reduce an Error to a string */
+function reduceError (error = {}) {
+  const response = error.response
+  if (response) {
+    if (response.status && response.statusText && response.body) {
+      return `${response.status} - ${response.statusText} (${JSON.stringify(response.body)})`
+    }
+  }
+
+  return error
+}
+
+function createRequestOptions ({ tenantId, apiKey, accessToken, body = {} }) {
+  return {
+    requestBody: body,
+    securities: {
+      authorized: {
+        BearerAuth: { value: accessToken },
+        ApiKeyAuth: { value: apiKey }
+      }
+    }
+  }
+}
+
+function requestInterceptor (req) {
+  logger.debug(`REQUEST:\n ${JSON.stringify(req, null, 2)}`)
+  return req
+}
+
+function responseInterceptor (res) {
+  logger.debug(`RESPONSE:\n ${JSON.stringify(res, null, 2)}`)
+  if (res.ok) {
+    const text = res.text.toString('utf-8')
+    try {
+      logger.debug(`DATA\n, ${JSON.stringify(JSON.parse(text), null, 2)}`)
+    } catch (e) {
+      logger.debug(`DATA\n ${text}`)
+    }
+  }
+  return res
+}
+
+module.exports = {
+  createRequestOptions,
+  requestInterceptor,
+  responseInterceptor,
+  reduceError
+}
---
+++ src/index.js
@@ -0,0 +1,119 @@
+/*
+Copyright 2019 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+/**
+ * {{LIB_NAME}} SDK
+ */
+
+'use strict'
+
+const Swagger = require('swagger-client')
+const loggerNamespace = '@{{REPO}}'
+const logger = require('@adobe/aio-lib-core-logging')(loggerNamespace, { level: process.env.LOG_LEVEL })
+const { reduceError, requestInterceptor, responseInterceptor, createRequestOptions } = require('./helpers')
+const { codes } = require('./SDKErrors')
+
+function init (tenantId, apiKey, accessToken) {
+  return new Promise((resolve, reject) => {
+    const clientWrapper = new LibNameCoreAPI()
+
+    clientWrapper.init(tenantId, apiKey, accessToken)
+      .then(initializedSDK => {
+        logger.debug('sdk initialized successfully')
+        resolve(initializedSDK)
+      })
+      .catch(err => {
+        logger.debug(`sdk init error: ${err}`)
+        reject(err)
+      })
+  })
+}
+
+/**
+* This class provides methods to call your {{LIB_NAME}} APIs.
+* Before calling any method initialize the instance by calling the `init` method on it
+* with valid values for tenantId, apiKey and accessToken
+*/
+class LibNameCoreAPI {
+  /**
+   * Initializes a LibNameCoreAPI object and returns it.
+   *
+   * @param {string} tenantId the tenant id
+   * @param {string} apiKey the API key for your integration
+   * @param {string} accessToken the access token for your integration
+   * @returns {LibNameCoreAPI}
+   */
+  async init (tenantId, apiKey, accessToken) {
+    // init swagger client
+    const spec = require('../spec/api.json')
+    const swagger = new Swagger({
+      spec: spec,
+      requestInterceptor,
+      responseInterceptor,
+      usePromise: true
+    })
+    this.sdk = (await swagger)
+
+    const initErrors = []
+    if (!tenantId) {
+      initErrors.push('tenantId')
+    }
+    if (!apiKey) {
+      initErrors.push('apiKey')
+    }
+    if (!accessToken) {
+      initErrors.push('accessToken')
+    }
+
+    if (initErrors.length) {
+      const sdkDetails = { tenantId, apiKey, accessToken }
+      throw new codes.ERROR_SDK_INITIALIZATION({ sdkDetails, messageValues: `${initErrors.join(', ')}` })
+    }
+
+    /** the tenant id */
+    this.tenantId = tenantId
+    /** the api key from your integration */
+    this.apiKey = apiKey
+    /** the access token from your integration */
+    this.accessToken = accessToken
+    return this
+  }
+
+  __createRequestOptions ({ body } = {}) {
+    return createRequestOptions({
+      tenantId: this.tenantId,
+      apiKey: this.apiKey,
+      accessToken: this.accessToken,
+      body
+    })
+  }
+
+  /**
+   * Get something.
+   *
+   * @param {Object} [parameters={}] parameters to pass
+   */
+  getSomething (parameters = {}) {
+    const sdkDetails = { parameters }
+
+    return new Promise((resolve, reject) => {
+      this.sdk.apis.mytag.getSomething(parameters, this.__createRequestOptions())
+        .then(response => {
+          resolve(response)
+        })
+        .catch(err => {
+          reject(new codes.ERROR_GET_SOMETHING({ sdkDetails, messageValues: reduceError(err) }))
+        })
+    })
+  }
+}
+module.exports = {
+  init: init
+}
